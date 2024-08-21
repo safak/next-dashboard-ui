f2827a8aa539cf67818a7f37636fad52b122fed9
Lansarbac2020 <secretariat.lansarcenter@gmail.com>
2024-08-21 16:12:33 +0300

    leachers list

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['images.pexels.com'],
+      },
+};
 
 export default nextConfig;
