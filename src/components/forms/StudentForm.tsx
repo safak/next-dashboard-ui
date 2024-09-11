83653992c6838a72d66e22d55b44fa567c487898
Sheldon Debra <kofisheldon@gmail.com>
2024-09-11 06:33:45 +0000

    updated

--- src/components/forms/StudentForm.tsx
+++ src/components/forms/StudentForm.tsx
@@ -10,7 +10,7 @@ const schema = z.object({
   username: z
     .string()
     .min(3, { message: "Username must be at least 3 characters long!" })
-    .max(20, { message: "Username must be at most 20 characters long!" }),
+    .max(5, { message: "Username must be at most 20 characters long!" }),
   email: z.string().email({ message: "Invalid email address!" }),
   password: z
     .string()
