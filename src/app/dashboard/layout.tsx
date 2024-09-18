9a63394b03d829653ff3fa69f378b712a41c3f49
siphonhata <siphonhata@gmail.com>
2024-09-18 23:56:37 +0200

    page logo

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
@@ -6,7 +9,12 @@ export default function DashboardLayout({
   return (  
       <div className="h-screen flex">
         {/* LEFT */}
-        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">l</div>
+        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
+          <Link href="/" className="flex items-center justify-center gap-2">
+            <Image src="/logo.png" alt="logo" width={32} height={32} />
+            <span className="hidden lg:block">Abuti Small</span>
+          </Link>
+        </div>
 
         {/* RIGHT */}
         <div className="w-[14%]  md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">r</div>
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,7 @@
+const NavBar = () => {
+  return (
+    <div className=''>NavBar</div>
+  )
+}
+
+export default NavBar
