767e4d6de6df6e5b62848bf6f8efcacd6ad0a070
siphonhata <siphonhata@gmail.com>
2024-09-19 14:14:58 +0200

    side menu bar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
+import Menu from "@/components/Menu";
+import NavBar from "@/components/NavBar";
 
 export default function DashboardLayout({
   children,
@@ -11,15 +13,19 @@ export default function DashboardLayout({
       <div className="h-screen flex">
         
         {/* LEFT */}
-        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
-          <Link href="/" className="flex items-center justify-center gap-2">
+        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+          <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
             <Image src="/logo.png" alt="logo" width={32} height={32} />
             <span className="hidden lg:block">Abuti Small</span>
           </Link>
+          <Menu />
         </div>
 
         {/* RIGHT */}
-        <div className="w-[14%]  md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">r</div>
+        <div className="w-[14%]  md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+          <NavBar />
+          {children}
+        </div>
         </div>
     
   );
