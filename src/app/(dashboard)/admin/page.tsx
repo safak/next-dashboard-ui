55697b46fa5b04dbbda713185ec8842e51ab72a7
M. Aktaruzzaman Joti <aktaruzzamanjoti97@gmail.com>
2024-09-28 01:44:30 +0600

    student chart done

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,10 +1,11 @@
+import CountCharts from "@/components/CountCharts";
 import UserCard from "@/components/UserCard";
 
 const AdminPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* LEFT */}
-      <div className="w-full lg:w-2/3">
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
         {/* USER CARD */}
         <div className="flex gap-4 justify-between flex-wrap">
           <UserCard type="student" />
@@ -12,6 +13,17 @@ const AdminPage = () => {
           <UserCard type="parent" />
           <UserCard type="staff" />
         </div>
+        {/* MIDDLE CHARTS */}
+        <div className="flex gap-4 flex-col lg:flex-row ">
+          {/* COUNT CHART */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountCharts />
+          </div>
+          {/* ATTENDANCE CHART */}
+          <div className="w-full lg:w-2/3 h-[450px]"></div>
+        </div>
+        {/* BOTTOM CHARTS */}
+        <div></div>
       </div>
       {/* RIGHT */}
       <div className="w-full lg:w-1/3">r</div>
---
+++ src/components/CountCharts.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import Image from "next/image";
+import { RadialBar, RadialBarChart, ResponsiveContainer } from "recharts";
+
+const data = [
+  {
+    name: "Total",
+    count: 106,
+    fill: "white",
+  },
+  {
+    name: "Girls",
+    count: 53,
+    fill: "#fae27c",
+  },
+  {
+    name: "Boys",
+    count: 53,
+    fill: "#ceebfa",
+  },
+];
+
+const CountCharts = () => {
+  return (
+    <div className="bg-white rounded-xl w-full h-full p-4">
+      {/* TITLE */}
+      <div className="flex justify-between items-center">
+        <h1 className="text-lg font-semibold">Students</h1>
+        <Image src="/moreDark.png" alt="" width={20} height={20} />
+      </div>
+      {/* CHART */}
+      <div className="relative w-full h-[75%]">
+        <ResponsiveContainer>
+          <RadialBarChart
+            cx="50%"
+            cy="50%"
+            innerRadius="40%"
+            outerRadius="100%"
+            barSize={32}
+            data={data}
+          >
+            <RadialBar background dataKey="count" />
+          </RadialBarChart>
+        </ResponsiveContainer>
+        <Image
+          src="/maleFemale.png"
+          alt=""
+          width={50}
+          height={50}
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+        />
+      </div>
+      {/* BOTTOM */}
+      <div className="flex justify-center gap-16">
+        <div className="flex flex-col gap-1">
+          <div className="w-5 h-5 bg-jotiSky rounded-full"></div>
+          <h1 className="font-bold">1234</h1>
+          <h2 className="text-xs text-gray-300">Boys (55%)</h2>
+        </div>
+        <div className="flex flex-col gap-1">
+          <div className="w-5 h-5 bg-jotiYellow rounded-full"></div>
+          <h1 className="font-bold">1234</h1>
+          <h2 className="text-xs text-gray-300">Girls (55%)</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CountCharts;
