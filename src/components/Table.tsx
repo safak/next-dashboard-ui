dea9fb7974c8354991a87a3879010078a69d308b
gantzoulis <gantzoulis@hotmail.com>
2024-10-09 15:36:02 +0300

    Teachers List design

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,7 +1,27 @@
-const Table = () => {
+const Table = (
+  {
+    columns,
+    renderRow,
+    data
+  }:{
+    columns:{header:string; accessor:string; className?:string;}[];
+    renderRow:(item:any)=> React.ReactNode;
+    data:any[];
+  }) => {
   return (
    <table className="w-full mt-4">
-        <th></th>
+        <thead>
+          <tr className="text-left text-gray-500 text-sm">
+            {columns.map(col=>(
+              <th key={col.accessor} className={col.className}>{col.header}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map((item)=>renderRow(item))
+          }
+        </tbody>
    </table>
   )
 }
