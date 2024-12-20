3583875a1d30d49407657e7a7dcd391cb17d5ec3
KanathipP <miniexor@gmail.com>
2024-12-20 08:31:18 +0700

    student dashboard design

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -10,7 +10,7 @@ const DashboradLayout = ({
   return (
     <div className="h-screen flex">
       {/* LEFT */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4 overflow-auto">
         <Link href="/" className="flex items-center justify-center gap-2">
           <Image src="/logo.png" alt="logo" width={32} height={32} />
           <span className="hidden lg:block">LILSchoolX</span>
