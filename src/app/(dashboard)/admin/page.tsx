b71bcbc4c4f2e8505bd660ddd5ea45ce01b1ade0
KanathipP <miniexor@gmail.com>
2024-12-20 07:29:17 +0700

    Calendar

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
+import EventCalendar from "@/components/EventCalendar";
 import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 
@@ -34,7 +35,9 @@ const AdminPage = () => {
         </div>
       </div>
       {/* RIGHT */}
-      <div className="w-full lg:w-1/3">r</div>
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalendar/>
+      </div>
     </div>
   );
 };
---
+++ src/components/Announcements.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Announcements = () => {
+  return (
+    <div>Announcements</div>
+  )
+}
+
+export default Announcements
