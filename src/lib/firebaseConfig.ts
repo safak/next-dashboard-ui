6aadbff7c54b8bf9107fbec2f0c77c0aa558fa39
Kenyang <kenyanglual05@gmail.com>
2024-12-25 20:03:42 -0500

    firebase initiation

---
+++ src/lib/firebaseConfig.ts
@@ -0,0 +1,17 @@
+ // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  };
+  console.log("Firebase API Key:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
