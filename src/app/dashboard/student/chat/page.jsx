64a2fd6e0a13c33b4ba732ab7452820ee8d9de20
David Juarez <85270302+djuarez2652@users.noreply.github.com>
2024-12-25 19:57:29 -0500

    refactor: delete duplicate chat directory (#3)

--- src/app/dashboard/student/chat/page.jsx
+++ src/app/dashboard/student/chat/page.jsx
@@ -1,20 +1,69 @@
-import SearchBar from "../../../../components/SearchBar";
+'use client';
 import Image from "next/image";
 import Robot from "/public/robot-chatbot.png";
+import SearchBar from "/src/components/SearchBar";
+import { useState } from "react";
+import MessageBox from "/src/components/MessageBox";
+import ReactLoading from 'react-loading';
 
+export default function ChatPage() {
+    const [messages, setMessages] = useState([]);
+    const [currMessage, setCurrMessage] = useState("");
+    const [loading, setLoading] = useState(false);
 
-export default function ChatPage(props) {
+    const handleSend = async () => {
+
+        setLoading(true);
+
+        const response = await fetch("/api/chat", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ message: currMessage })
+        });
+
+        const data = await response.json();
+        console.log(data.message.content)
+        setCurrMessage("");
+        setMessages((prev) => [...prev, currMessage, data.message.content]);
+        setLoading(false);
+    };
+
     return (
-        <div className="w-full h-full flex justify-center items-center">
-            <div
-                className="w-full h-full flex flex-col items-center justify-center"
-            >
+        <div className="w-full h-[90%] flex flex-col justify-between">
+            {/* MessageBox Section */}
+            <div className="h-[90%] w-full flex justify-center items-start">
+                <div className="w-2/3 h-full">
+                    {
+                        loading ?
+                            <div
+                                className="h-full w-full flex justify-center items-center"
+                            >
+                                <ReactLoading type={'bubbles'} color={'black'} />
+                            </div> :
+                            messages.length > 0 ?
+                                <MessageBox messages={messages} /> :
+                                <div className="h-full flex justify-center items-center text-gray-500">
+                                    No messages...
+                                </div>
+                    }
+                </div>
+            </div>
+
+            {/* Input Section */}
+            <div className="h-fit self-end w-full flex justify-center items-center">
                 <Image
                     src={Robot}
-                    className="w-48 h-48"
+                    className="w-32 h-32"
+                    alt="Robot Image"
                 />
-                <SearchBar />
+                <SearchBar
+                    handleSend={handleSend}
+                    value={currMessage}
+                    setValue={setCurrMessage}
+                />
             </div>
         </div>
-    )
+    );
 }
