bdc798a439aaff103d862eb3a3187a8e9dc21bed
lucidcity <ibrahimy437@gmail.com>
2024-12-29 23:51:13 +0100

    feat(chart): add AttendanceChart component for visualizing attendance data

---
+++ src/components/AttendanceChart.tsx
@@ -0,0 +1,63 @@
+"use client"
+
+import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+const data = [
+  {
+    name: 'Mon',
+    present: 60,
+    absent: 40,
+  },
+  {
+    name: 'Tue',
+    present: 70,
+    absent: 60,
+  },
+  {
+    name: 'Wed',
+    present: 90,
+    absent: 75,
+  },
+  {
+    name: 'Thu',
+    present: 90,
+    absent: 75,
+  },
+  {
+    name: 'Fri',
+    present: 65,
+    absent: 55,
+  },
+];
+
+import React from 'react'
+import Image from 'next/image';
+
+const AttendanceChart = () => {
+  return (
+    <div className='bg-white rounded-lg p-4 h-full'>
+        <div className="flex justify-between items-center">
+            <h1 className="text-lg font-semibold">Attendance</h1>
+            <Image src="/moreDark.png" alt="" width={20} height={20}/>
+        </div>
+        <ResponsiveContainer width="100%" height="90%">
+        <BarChart
+          width={500}
+          height={300}
+          data={data}
+          barSize={20}
+        >
+          <CartesianGrid strokeDasharray="3 3" vertical={false} stroke='#ddd'/>
+          <XAxis dataKey="name" axisLine={false} tick={{fill:"#d1d5db"}} tickLine={false}/>
+          <YAxis axisLine={false} tick={{fill:"#d1d5db"}} tickLine={false}/>
+          <Tooltip contentStyle={{borderRadius:"10px",borderColor:"lightgray"}}/>
+          <Legend align='left' verticalAlign='top' wrapperStyle={{paddingTop:"20px", paddingBottom:"40px"}}/>
+          <Bar dataKey="present" fill="#FAE27C" activeBar={<Rectangle fill="pink" stroke="blue" />} legendType='circle' radius={[10,10,0,0]}/>
+          <Bar dataKey="absent" fill="#C3E8FA" activeBar={<Rectangle fill="gold" stroke="purple" />} legendType='circle' radius={[10,10,0,0]}/>
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  )
+}
+
+export default AttendanceChart
