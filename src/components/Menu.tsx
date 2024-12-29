fe136e9776209f240ded0cfac56c0467d0642299
lucidcity <ibrahimy437@gmail.com>
2024-12-29 09:07:13 +0100

    Add sidebar menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -112,3 +112,27 @@ const menuItems = [
     ],
   },
 ];
+
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div className="mt-4 text-sm">
+      {menuItems.map((menu, index) => (
+        <div key={index} className="flex flex-col gap-2">
+          <span className="hidden lg:block text-gray-400 font-light my-4">{menu.title}</span>
+            {menu.items.map((item, index) => (
+              <Link key={index} href={item.href} className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2">
+                <Image src={item.icon} alt={item.label} width={20} height={20} />
+                <span className="hidden lg:block text-gray-400 text-sm">{item.label}</span>
+              </Link>
+            ))}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Menu
