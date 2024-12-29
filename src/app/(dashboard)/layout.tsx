fbe183cfca2f849875001b83c24297969bd7895d
lucidcity <ibrahimy437@gmail.com>
2024-12-29 08:58:49 +0100

    Add sidebar and navbar to dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,8 @@
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
@@ -5,11 +10,16 @@ export default function DashboardLayout({
 }>) {
   return <div className="h-screen flex">
     {/** Sidebar */}
-    <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-gray-800 text-white">
-      Sidebar
+    <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] text-white p-4">
+      <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
+        <Image src="/logo.png" alt="Logo" width={32} height={32} />
+        <span className="hidden lg:block">Gifted</span>
+      </Link>
+      <Menu/>
     </div>
     {/** Content */}
-    <div className="w-[86%] md:w-[92%] lg:[84%] xl:w-[86%] bg-gray-200">
+    <div className="w-[86%] md:w-[92%] lg:[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+      <Navbar />
       {children}
     </div>
   </div>
