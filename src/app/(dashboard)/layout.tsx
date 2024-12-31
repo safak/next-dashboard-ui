a5a4e947e954f8f943e6bc6169bf6ad85c3a5fab
lucidcity <ibrahimy437@gmail.com>
2024-12-31 09:39:49 +0100

    feat(layout): update sidebar styles for improved visibility

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -10,10 +10,10 @@ export default function DashboardLayout({
 }>) {
   return <div className="h-screen flex">
     {/** Sidebar */}
-    <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] text-white p-4">
+    <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
       <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
         <Image src="/logo.png" alt="Logo" width={32} height={32} />
-        <span className="hidden lg:block">Gifted</span>
+        <span className="hidden lg:block font-bold">Gifted</span>
       </Link>
       <Menu/>
     </div>
