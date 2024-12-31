e7132839c537171229651b2c3f7a58aa5d574b53
lucidcity <ibrahimy437@gmail.com>
2024-12-31 09:40:33 +0100

    feat(admin): add Announcements component to AdminPage

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,4 @@
+import Announcements from '@/components/Announcements'
 import AttendanceChart from '@/components/AttendanceChart'
 import CountChart from '@/components/CountChart'
 import EventCalendar from '@/components/EventCalendar'
@@ -36,6 +37,7 @@ const AdminPage = () => {
       {/* Right side */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
         <EventCalendar />
+        <Announcements />
       </div>
     </div>
   )
