a93b5a7c9d50aea48362cb4d9cd9ca24f9519d6f
lucidcity <ibrahimy437@gmail.com>
2025-01-02 09:42:48 +0100

    feat(parent): integrate Announcements and BigCalendar components into ParentPage layout

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,8 +1,22 @@
+import Announcements from '@/components/Announcements'
+import BigCalendar from '@/components/BigCalendar'
 import React from 'react'
 
 const ParentPage = () => {
   return (
-    <div>ParentPage</div>
+    <div className='flex-1 p-4 flex gap-4 flex-col xl:flex-row'>
+      {/* Left side */}
+      <div className='w-full xl:2/3'>
+        <div className='h-full bg-white p-4 rounded-md'>
+          <h1 className='text-xl font-semibold'>Schedule (John Doe)</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* Right side */}
+      <div className='w-full xl:1/3'>
+        <Announcements />
+      </div>
+    </div>
   )
 }
 
