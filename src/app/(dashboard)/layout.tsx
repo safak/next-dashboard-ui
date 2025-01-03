7a05449b1b06069317f8f6d2c365b9a5c2746e2d
lucidcity <ibrahimy437@gmail.com>
2025-01-03 12:28:27 +0100

    Add "flex col" class

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -18,7 +18,7 @@ export default function DashboardLayout({
       <Menu/>
     </div>
     {/** Content */}
-    <div className="w-[86%] md:w-[92%] lg:[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+    <div className="w-[86%] md:w-[92%] lg:[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
       <Navbar />
       {children}
     </div>
