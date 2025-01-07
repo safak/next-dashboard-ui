fdf142e19fc4bc89a49eb374aedf23eca5ac1639
lucidcity <ibrahimy437@gmail.com>
2025-01-07 08:41:39 +0100

    feat(events): integrate FormModal for create, update, and delete actions in EventListPage

--- src/app/(dashboard)/list/events/page.tsx
+++ src/app/(dashboard)/list/events/page.tsx
@@ -1,3 +1,4 @@
+import FormModal from '@/components/FormModal'
 import Pagination from '@/components/Pagination'
 import Table from '@/components/Table'
 import TableSearch from '@/components/TableSearch'
@@ -37,14 +38,11 @@ const EventListPage = () => {
       <td className='hidden md:table-cell'>{item.endTime}</td>
       <td>
         <div className='flex items-center gap-2'>
-          <Link href={`/list/teachers/${item.id}`}>
-            <button className='w-7 h-7 flex items-center justify-center rounded-full bg-mySky'>
-              <Image src='/edit.png' alt='' width={16} height={16} />
-            </button>
-          </Link>
-          {role === "admin" && (<button className='w-7 h-7 flex items-center justify-center rounded-full bg-myPurple'>
-              <Image src='/delete.png' alt='' width={16} height={16} />
-            </button>
+          {role === "admin" && (
+            <>
+            <FormModal table='event' type='update' data={item}/>
+            <FormModal table='event' type='delete' id={item.id}/>
+            </>
           )}
         </div>
       </td>
@@ -65,9 +63,9 @@ const EventListPage = () => {
             <button className='w-8 h-8 flex items-center justify-center rounded-full bg-myYellow'>
               <Image src='/sort.png' alt=''width={14} height={14}/>
             </button>
-            {role === "admin" && <button className='w-8 h-8 flex items-center justify-center rounded-full bg-myYellow'>
-              <Image src='/plus.png' alt=''width={14} height={14}/>
-            </button>}
+            {role === "admin" &&
+              <FormModal table='event' type='create'/>
+            }
           </div>
         </div>
       </div>
