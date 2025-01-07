afffa527aa1bfcd1f41a9b929472a1415a909d9e
lucidcity <ibrahimy437@gmail.com>
2025-01-07 08:41:04 +0100

    feat(assignments): integrate FormModal for create, update, and delete actions in AssignmentListPage

--- src/app/(dashboard)/list/assignments/page.tsx
+++ src/app/(dashboard)/list/assignments/page.tsx
@@ -1,3 +1,4 @@
+import FormModal from '@/components/FormModal'
 import Pagination from '@/components/Pagination'
 import Table from '@/components/Table'
 import TableSearch from '@/components/TableSearch'
@@ -34,14 +35,11 @@ const AssignmentListPage = () => {
       <td className='hidden md:table-cell'>{item.dueDate}</td>
       <td>
         <div className='flex items-center gap-2'>
-          <Link href={`/list/teachers/${item.id}`}>
-            <button className='w-7 h-7 flex items-center justify-center rounded-full bg-mySky'>
-              <Image src='/edit.png' alt='' width={16} height={16} />
-            </button>
-          </Link>
-          {role === "admin" && (<button className='w-7 h-7 flex items-center justify-center rounded-full bg-myPurple'>
-              <Image src='/delete.png' alt='' width={16} height={16} />
-            </button>
+          {role === "admin" && (
+            <>
+            <FormModal table='assignment' type='update' data={item}/>
+            <FormModal table='assignment' type='delete' id={item.id}/>
+            </>
           )}
         </div>
       </td>
@@ -62,9 +60,9 @@ const AssignmentListPage = () => {
             <button className='w-8 h-8 flex items-center justify-center rounded-full bg-myYellow'>
               <Image src='/sort.png' alt=''width={14} height={14}/>
             </button>
-            {role === "admin" && <button className='w-8 h-8 flex items-center justify-center rounded-full bg-myYellow'>
-              <Image src='/plus.png' alt=''width={14} height={14}/>
-            </button>}
+            {role === "admin" &&
+            <FormModal table='assignment' type='create'/>
+            }
           </div>
         </div>
       </div>
